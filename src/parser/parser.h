32fcfd92bb1475c430a31dd56d6d48afc0e03e6b
provemsu <nikita-burg@mail.ru>
2026-02-23 12:31:31 +0300

    add: using

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,6 +1,8 @@
 #include <fstream>
 #include <stdexcept>
 
+using Data = std::vector<std::pair<std::string, int>>;
+
 class ParserException : public std::runtime_error {
 public:
   explicit ParserException(const std::string &msg) : std::runtime_error(msg) {}
@@ -10,14 +12,12 @@ class Parser { // TODO IParser
 private:
   const std::string filename_;
   std::ifstream is_;
-  std::vector<std::pair<std::string, int>> data_;
+  Data data_;
   void openFile();
   bool isOpen();
 
 public:
   Parser(const std::string &filename);
   bool parseFile();
-  const std::vector<std::pair<std::string, int>> &getData() const {
-    return data_;
-  }
+  const Data &getData() const { return data_; }
 };
